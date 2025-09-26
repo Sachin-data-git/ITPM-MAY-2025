c1788fa2e8ab3f0cc31f18cf52c8fbec6e258c9d
sachin <sachin.g@itpreneurpune.com>
2025-09-26 17:26:04 +0530

    Task Added

---
+++ Javascript/ImportExport/script1.js
@@ -0,0 +1,8 @@
+let fname="Ajay";
+let sname="Tambe" 
+
+
+//single
+// export default name;
+
+export  {fname,sname};
---
+++ Javascript/ImportExport/script2.js
@@ -0,0 +1,9 @@
+
+import {fname,sname} from "./script1.js";
+console.log(fname);
+console.log(sname);
+
+
+localStorage.setItem("theme","black");
+console.log(localStorage.getItem("theme"));
+
